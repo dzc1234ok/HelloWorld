543adbb525a4874d65377df5884cf4a1b0dab9d1
Duan <duan@duandemacbook-pro.local>
2019-04-04 22:53:22 +0800

    thread

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -5,11 +5,13 @@
 //  Created by Duan on 2019/4/3.
 //  Copyright © 2019 Duan. All rights reserved.
 //
-
+#include <unistd.h>
 #include <iostream>
+#include <thread>
+void hello(int i);
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+int main(int argc, const char * argv[])
+{
     std::cout << "Hello, World!\n";
     return 0;
 }
